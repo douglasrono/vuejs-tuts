2440554d42ba70208bf48305c4b22a031eaa17c4
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2023-01-22 11:44:36 +0300

    Create binding.vue

---
+++ binding.vue
@@ -0,0 +1,15 @@
+
+<div id="app">
+{{ message }}
+</div>
+
+<script>
+var myObject = new Vue({
+    el: '#app',
+    data: {message: 'Hello Vue!'}
+})
+
+function myFunction() {
+    myObject.message = "John Doe";
+}
+</script>
