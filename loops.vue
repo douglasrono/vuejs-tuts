667bd7e5bea8b9c46e1e46b28a340d7d15c5fa44
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2023-01-22 11:36:35 +0300

    Create loops.vue

---
+++ loops.vue
@@ -0,0 +1,21 @@
+
+<div id="app">
+ <ul>
+   <li v-for="x in todos">
+   {{ x.text }}
+   </li>
+  </ul>
+</div>
+
+<script>
+myObject = new Vue({
+    el: '#app',
+    data: {
+    todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue.js' },
+        { text: 'Build Something Awesome' }
+        ]
+    }
+})
+</script>
